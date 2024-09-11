e41bd42e0f71fc055598992fea40fc4afdaaa29b
Md Mohsin <108623805+mdmohsin6@users.noreply.github.com>
2024-09-11 13:18:06 +0530

    episode1

---
+++ App.js
@@ -0,0 +1,9 @@
+const heading = react.createElement(
+    "h1",
+     {id: "heading"},
+      "hello"
+    );
+
+const root= reactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
